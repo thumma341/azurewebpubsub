3acc3d16564ad84ab202904cb1a421e4a03ecea2
karthik_nalla <karthiksappi2@gmail.com>
2022-02-18 09:42:00 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AzureKeyCredential, WebPubSubServiceClient } from '@azure/web-pubsub';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'azurewebpubsub';
+  cred = new AzureKeyCredential('4W+BtG66BheERI6NGTT2vJV/UGkt/kWbnQIjNnEcXE8=');
+  endpoint = 'https://rajwebpubsubdemo.webpubsub.azure.com';
+
+  constructor() { }
+
+  async ngOnInit(): Promise<void> {
+    this.subscribeNotification();
+  }
+
+  async subscribeNotification() {
+    const serviceClient = new WebPubSubServiceClient(this.endpoint, this.cred, 'raj');
+   // new WebPubSubServiceClient()
+   // const serviceClient = new WebPubSubServiceClient(this.endpoint, this.cred, 'Hub');
+    let token = await serviceClient.getClientAccessToken();
+    let ws = new WebSocket(token.url);
+    ws.onmessage = function (e) {
+      var server_message = e.data;
+      console.log(server_message);
+    }
+  };
 }
